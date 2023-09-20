e4a871ac204168e00f0c7fee8931dec5529f80f5
yoonyeorin <yeolin3579@naver.com>
2023-09-20 19:47:09 +0900

    2023-09-20

---
+++ C세미나/계산기.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%d + %d = %d\n",a, b, a + b);
+	printf("%d - %d = %d\n",a, b, a - b);
+	printf("%d * %d = %d\n",a, b,  a * b);
+	printf("%d / %d = %d\n",a, b, a / b);
+	printf("%d %% %d = %d\n",a, b, a % b);
+
+	return 0;
+}
---
+++ C세미나/자료형.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 5;
+	double b = 4.5;
+	float c = 5.2;
+	char d = 'a';
+
+	printf("%d %.1lf %.1f %c\n", a, b, c, d);
+
+	return 0;
+}
